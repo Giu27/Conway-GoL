d424f7dea958dcbc354f94ec65d255bcd07b590d
Giu27 <Caruso.giuseppe09@hotmail.com>
2025-07-08 19:26:51 +0200

    game: pause and clean the grid with space and backspace

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pygame as pg, sys, random
 from settings import *
 
-live_cells = 5
-
 def draw_on_screen(screen,tile_grid, comp_grid):
-    cell_size = (WIDTH/TILE_GRID_WIDTH,HEIGHT/TILE_GRID_HEIGHT)
+    cell_size = ((WIDTH/TILE_GRID_WIDTH) - SIZE_OFFSET ,(HEIGHT/TILE_GRID_HEIGHT) - SIZE_OFFSET)
     for row in range(TILE_GRID_HEIGHT):
         if comp_grid:
             if tile_grid[row] == comp_grid[row]:continue
@@ -12,11 +10,16 @@ def draw_on_screen(screen,tile_grid, comp_grid):
             cell_color = DEFAULT_COLORS[tile_grid[row][column]]
             cell_surface = pg.Surface(cell_size)
             cell_surface.fill(cell_color)
-            cell_rect = cell_surface.get_frect(topleft = (cell_size[0] * column, cell_size[1] * row))
+            cell_rect = cell_surface.get_frect(topleft = (cell_size[0] * column + SIZE_OFFSET, cell_size[1] * row + SIZE_OFFSET))
             screen.blit(cell_surface,cell_rect)
 
+def clear_grid(tile_grid):
+    for i in range(TILE_GRID_HEIGHT):
+        for j in range(TILE_GRID_WIDTH):
+            tile_grid[i][j] = 0
+    draw_on_screen(screen,tile_grid, [])
+
 def update_cell(tile_grid,x,y):
-    global live_cells
     cell_state = tile_grid[y][x]
     neighbours = 0
     coords = (x - 1, y - 1)
@@ -29,14 +32,11 @@ def update_cell(tile_grid,x,y):
     if cell_state:
         if neighbours < 2: 
             cell_state = 0
-            live_cells -= 1
         elif neighbours > 3: 
             cell_state = 0
-            live_cells -= 1
     else:
         if neighbours == 3: 
             cell_state = 1
-            live_cells += 1
     return cell_state
 
 def update_tile_grid(tile_grid):
@@ -50,30 +50,45 @@ def update_tile_grid(tile_grid):
 pg.init()
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Conway's Game of Life")
+pg.display.set_caption("Conway's Game of Life: paused")
 
 clock = pg.time.Clock()
 
 prev_tile_grid = [[0 for _ in range(TILE_GRID_WIDTH)] for _ in range(TILE_GRID_HEIGHT)]
 for i in range(1000):
-    live_cells += 1
     coords = (random.randint(0,TILE_GRID_HEIGHT - 1),random.randint(0,TILE_GRID_WIDTH - 1))
     prev_tile_grid[coords[0]][coords[1]] = 1
 
 draw_on_screen(screen,prev_tile_grid, [])
+
+paused = True
 
 while (True):
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
-    new_tile_grid = update_tile_grid(prev_tile_grid)
-    draw_on_screen(screen,new_tile_grid,prev_tile_grid)
-    prev_tile_grid = new_tile_grid.copy()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                paused = not paused
+                if paused: pg.display.set_caption("Conway's Game of Life: paused")
+                else: pg.display.set_caption("Conway's Game of Life: running")
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LCTRL:
+                DEBUG = not DEBUG
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_BACKSPACE:
+                clear_grid(prev_tile_grid)
+    
+    if not paused:
+        screen.fill("black")
+        if DEBUG: screen.fill(DEBUG_BACKGROUND_COLOR)
+        new_tile_grid = update_tile_grid(prev_tile_grid)
+        draw_on_screen(screen,new_tile_grid,prev_tile_grid)
+        prev_tile_grid = new_tile_grid.copy()
 
     if DEBUG:
         print(f"FPS: {int(clock.get_fps())}")
-        print(f"CELLE VIVE: {live_cells}")
 
     clock.tick(FPS)
     pg.display.update()
